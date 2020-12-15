6e7d8056b9e9ac2347b31afb7202f62939fa93e0
singeypro <65811178+singeypro@users.noreply.github.com>
2020-12-15 15:59:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var ball;var database;
+var position;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    console.log(database);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "blue";
+    var ballPositionRef  = database.ref('ball/position');
+    ballPositionRef.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    
+    database.ref('ball/position').update({
+        x: position.x+x,
+        y: position.y+y
+    })
+}
+
+function readPosition(data){
+position = data.val();
+ball.x = position.x;
+ball.y = position.y;
+}
+
+function showError(){
+console.log("do a refresh if not working (:")
+}
